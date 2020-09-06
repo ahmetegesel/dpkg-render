f07b14da0793a108cab98b1822e3902123aab20b
Ahmet Egesel <ahmetegesel@hotmail.com>
2020-09-07 01:25:14 +0200

    chore: Changed the way name of the pckg is rendered

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 import express from 'express';
 import path from 'path';
-
 import './config';
-
 import DpkgStatus from './src/DpkgStatus';
 
 const app = express();
-
-const router = express.Router();
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 const dpkgStatus = new DpkgStatus();
-router.get('/', (req, res) => res.render('index', {
+app.get('/', (req, res) => res.render('index', {
   packages: dpkgStatus.getPackageNames(),
 }));
-router.get('/package/:name', (req, res) => res.render('package', {
-  name: req.params.name,
-  ...dpkgStatus.getPackage(req.params.name),
-}));
-
-app.use('/', router);
+app.get(
+  '/package/:name',
+  (req, res) => res.render('package', dpkgStatus.getPackage(req.params.name)),
+);
 
 // eslint-disable-next-line no-console
 app.listen(8080, () => console.log('localhost:8080'));
